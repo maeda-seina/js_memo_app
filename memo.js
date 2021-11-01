fbcd037545eda079499dbb79312f7c8686b06951
maeda-seina <ayrton19601027@gmail.com>
2021-11-01 13:43:32 +0900

    memoディレクトリ以下のjsonファイルが一つだけの場合、lオプションで表示できるようになった。

--- memo.js
+++ memo.js
@@ -4,18 +4,31 @@ const uuid = generateUuid()
 const minimist = require('minimist')
 const argv = minimist(process.argv.slice(2))
 
-process.stdin.resume()
-process.stdin.setEncoding('utf8')
-const lines = []
-const reader = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-reader.on('line', (line) => {
-  lines.push(line)
-})
-reader.on('close', () => {
-  const memo = lines.join('\n')
-  fs.writeFileSync(`./${uuid}.json`, JSON.stringify({ memo: memo }))
-})
-console.log(argv)
+const createMemo = () => {
+  process.stdin.resume()
+  process.stdin.setEncoding('utf8')
+  const lines = []
+  const reader = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  reader.on('line', (line) => {
+    lines.push(line)
+  })
+  reader.on('close', () => {
+    const memo = lines.join('\n')
+    fs.writeFileSync(`./memo/${uuid}.json`, JSON.stringify({ memo: memo }))
+  })
+}
+
+const readMemo = () => {
+  const jsonFile = fs.readdirSync('memo/')
+  const memoContent = JSON.parse(fs.readFileSync(`./memo/${jsonFile}`))
+  const displayContent = memoContent.memo.split('\n')[0]
+  console.log(displayContent)
+}
+if (argv.l) {
+  readMemo()
+} else {
+  createMemo()
+}
